d19e1ab024e182b078c5e6c29a367fcb703542c3
mashaohua <mashaohua@autohome.com.cn>
2021-07-02 19:31:42 +0800

    斐波那契数列实现

---
+++ algorithms/src/main/java/com/shawntime/algorithms/fibonacci/FibonacciTest.java
@@ -0,0 +1,59 @@
+package com.shawntime.algorithms.fibonacci;
+
+import java.util.Stack;
+
+/**
+ * 斐波那契数列实现
+ * F(0)=0，F(1)=1, F(n)=F(n - 1)+F(n - 2)（n ≥ 2，n ∈ N*）
+ *
+ * 位置： 0 1 2 3 4 5 6 7
+ * 数列： 0 1 1 2 3 5 8 13
+ */
+public class FibonacciTest {
+
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(fib1(n));
+        System.out.println(fib2(n));
+        System.out.println(fib2(n));
+    }
+
+    public static int fib1(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib2(n - 1) + fib2(n - 2);
+    }
+
+    public static int fib2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int first = 0;
+        int second = 1;
+        for (int i = 0; i < n - 1; ++i) {
+            int sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return second;
+    }
+
+    /**
+     * 栈实现
+     */
+    public static int fib3(int n) {
+        Stack<Integer> stack = new Stack();
+        stack.push(0);
+        stack.push(1);
+        for (int i = 0; i < n - 1; ++i) {
+            Integer first = stack.pop();
+            Integer second = stack.pop();
+            int sum = first + second;
+            stack.push(second);
+            stack.push(sum);
+        }
+        return stack.peek();
+    }
+
+}
