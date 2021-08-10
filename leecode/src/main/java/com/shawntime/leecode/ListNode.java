a2a3fd8a97e19767b13d3b5ca91d7babd56e185a
mashaohua <mashaohua@autohome.com.cn>
2021-08-10 19:53:39 +0800

    删除链表中的节点

---
+++ leecode/src/main/java/com/shawntime/leecode/ListNode.java
@@ -0,0 +1,11 @@
+package com.shawntime.leecode;
+
+public class ListNode {
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ leecode/src/main/java/com/shawntime/leecode/_237_删除链表中的节点.java
@@ -0,0 +1,16 @@
+package com.shawntime.leecode;
+
+/**
+ * https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+ */
+public class _237_删除链表中的节点 {
+
+    /**
+     * 当前节点值替换为next节点值，并删除next节点
+     */
+    public void deleteNode(ListNode node) {
+        ListNode next = node.next;
+        node.val = next.val;
+        node.next = next.next;
+    }
+}
